6f6fe4577e556e40e7f340888436d0ed29ec200a
Demesh Fernando <wdemeshfernando@gmail.com>
2024-07-24 21:28:30 +0530

    Adjusting NavBar component and completing the basic implementation.

--- shopify_frtnd/src/configs/config.jsx
+++ shopify_frtnd/src/configs/config.jsx
@@ -1,8 +1,11 @@
 //import images
 import CART_ICON from "../assets/icons/cart-icon.png";
 import SEARCH_ICON from "../assets/icons/search-icon.png";
+import HAMBURGER_ICON from "../assets/icons/bars-solid.svg";
+
 import _1 from "../assets/products/1.png";
+
 import STAR_FILLED from "../assets/ratings/star-regular.svg";
 import STAR_EMPTY from "../assets/ratings/star-solid.svg";
 
-export { CART_ICON, SEARCH_ICON, _1, STAR_FILLED, STAR_EMPTY };
+export { CART_ICON, SEARCH_ICON, HAMBURGER_ICON, _1, STAR_FILLED, STAR_EMPTY };
