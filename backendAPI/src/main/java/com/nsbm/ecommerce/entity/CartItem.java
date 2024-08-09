177b8a33c64ea876c42f0dc6fd19af614b7c3584
ahamedafri <ahamedafricontact@gmail.com>
2024-08-09 22:02:57 +0530

    CartItem Entity Created

---
+++ backendAPI/src/main/java/com/nsbm/ecommerce/entity/CartItem.java
@@ -0,0 +1,30 @@
+package com.nsbm.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "cart_item")
+public class CartItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "cart_id")
+    private Cart cart;
+}
