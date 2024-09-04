28073922ccae933832b8d207b7b6cb7b9e525090
Dilan Tharaka <98627647+dilantharaka22759@users.noreply.github.com>
2024-09-04 23:19:13 +0530

    Create User Repository

--- backendAPI/src/main/java/com/nsbm/ecommerce/repository/UserRepository.java
+++ backendAPI/src/main/java/com/nsbm/ecommerce/repository/UserRepository.java
@@ -1,6 +1,5 @@
 package com.nsbm.ecommerce.repository;
 
-import com.nsbm.ecommerce.entity.Product;
 import com.nsbm.ecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
