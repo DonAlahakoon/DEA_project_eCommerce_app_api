5a70b4bda157ae23be2940f074e4bc983780574c
Dilan Tharaka <98627647+dilantharaka22759@users.noreply.github.com>
2024-09-05 09:31:46 +0530

    Create User Service

--- backendAPI/src/main/java/com/nsbm/ecommerce/services/UserService.java
+++ backendAPI/src/main/java/com/nsbm/ecommerce/services/UserService.java
@@ -1,4 +1,19 @@
 package com.nsbm.ecommerce.services;
 
+import com.nsbm.ecommerce.dto.obj.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+
+    UserDto addNewUser(UserDto userDto);
+
+    UserDto getUserById(int id);
+
+    List<UserDto> getAllUsers();
+
+    UserDto updateProduct(int userId,UserDto user);
+
+    void deleteUser(int id);
+
 }
