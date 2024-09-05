78134bc2fc55da88326fac71b33292bfa9431bf3
Dilan Tharaka <98627647+dilantharaka22759@users.noreply.github.com>
2024-09-05 08:47:43 +0530

    Create User Dto

--- backendAPI/src/main/java/com/nsbm/ecommerce/dto/obj/UserDto.java
+++ backendAPI/src/main/java/com/nsbm/ecommerce/dto/obj/UserDto.java
@@ -1,4 +1,23 @@
 package com.nsbm.ecommerce.dto.obj;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class UserDto {
+    private int id;
+    private String name;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
